91a0c0f1787f98a47f7fb09a3f56d1b445a76d0c
Prashant Bhat <prashantbhat84@gmail.com>
2025-06-30 11:30:30 +0530

    variables and constants

--- lesson_1/main.go
+++ lesson_1/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+	"unsafe"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Welcome to Go programming!")
-	fmt.Println("This is the first lesson.")
-	fmt.Println("Let's learn Go together!")
+	var a int32
+	var b bool
+	var c uint32
+	d := '%'
+	const myName = "Prashant Bhat"
+
+	fmt.Println((a))
+	fmt.Println("Size of int:", unsafe.Sizeof(a))
+	fmt.Println("Size of bool:", unsafe.Sizeof(b))
+	fmt.Println("Size of uint:", unsafe.Sizeof(c))
+	utf8Rune := utf8.RuneCountInString(string(d))
+	fmt.Println("Size of rune:", utf8Rune)
+	fmt.Println("Size of rune:", unsafe.Sizeof(d))
+	fmt.Println("Size of string:", unsafe.Sizeof(myName))
+	fmt.Println("Size of string:", len(myName))
+
 }
