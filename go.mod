68057dca87e68e36e149a4952df7bb9e98185e71
Prashant Bhat <prashantbhat84@gmail.com>
2025-06-30 11:06:43 +0530

    lesson1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang_tutorial
+
+go 1.21.5
---
+++ lesson_1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to Go programming!")
+	fmt.Println("This is the first lesson.")
+	fmt.Println("Let's learn Go together!")
+}
